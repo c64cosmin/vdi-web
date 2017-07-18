41d8c7de752840d6cc818929803a29221db88223
Cosmin Munteanu <cmunteanu@cloudbasesolutions.com>
2017-07-18 13:40:27 +0300

    Removed ununsed config parameters

--- src/main/java/com/cloudbase/vdi/Config.java
+++ src/main/java/com/cloudbase/vdi/Config.java
@@ -7,22 +7,14 @@ import org.json.JSONObject;
 
 public class Config {
     private static String authUrl = null;
-    private static String password = null;
     private static String projectDomainName = null;
     private static String projectName = null;
     private static String userDomainName = null;
-    private static String username = null;
 
     public static String getAuthUrl() {
         if (authUrl == null)
             Config.readConfig();
         return Config.authUrl;
-    }
-
-    public static String getPassword() {
-        if (password == null)
-            Config.readConfig();
-        return Config.password;
     }
 
     public static String getProjectDomainName() {
@@ -43,19 +35,11 @@ public class Config {
         return Config.userDomainName;
     }
 
-    public static String getUsername() {
-        if (username == null)
-            Config.readConfig();
-        return Config.username;
-    }
-
     public static void readConfig() {
         JSONObject json = null;
         try {
             json = new JSONObject(Utility.readFile("/etc/guacamole/vdi-client.json"));
             authUrl = json.getString("auth-url");
-            username = json.getString("username");
-            password = json.getString("password");
             projectName = json.getString("project-name");
             userDomainName = json.getString("user-domain-name");
             projectDomainName = json.getString("project-domain-name");
